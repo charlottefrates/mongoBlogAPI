442ba14ceb6a172f8dbbca79588eca0f50fb3eb7
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-10 09:37:02 -0700

    debugging 2

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -29,7 +29,7 @@ router.get('/posts', (req, res) => {
   .find()
   .exec()
   .then(posts => {
-    res.json({posts:posts.map((post) => post.apiRepr()));
+    res.json({posts:posts.map((post) => post.apiRepr())});
   })
   .catch(err => {
     console.error(err);
