986fcd7df41d51b2a3c9dd68d71973fdc471c44c
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-11 11:05:04 -0700

    debugging cont

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
+                      'mongodb://localhost/blog' ||;
+
+
+/*exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
                       'mongodb://localhost/blog' ||
-                      'mongodb://root:root@ds133221.mlab.com:33221/mongoblogapi';
+                      'mongodb://root:root@ds133221.mlab.com:33221/mongoblogapi';*/
+
+/*exports.DATABASE_URL = 'mongodb://root:root@ds133221.mlab.com:33221/mongoblogapi';*/
+
 exports.PORT = process.env.PORT || 8080;
