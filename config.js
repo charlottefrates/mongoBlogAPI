25a7362818f58086d6b28e377c430cb63b92d4bf
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-11 10:39:25 -0700

    config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog';
+                      'mongodb://localhost/blog' ||
+                      'mongodb://root:root@ds133221.mlab.com:33221/mongoblogapi';
 exports.PORT = process.env.PORT || 8080;
